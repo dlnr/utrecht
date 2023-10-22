952519c06b1f1281636ca2b2bdf4d48c38d76676
Robbert <mail@robbertbroersma.nl>
2023-10-22 09:40:56 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@b64dd6a7ffe6a83b9c17d196b92739e246a165ec 🚀

--- storybook-vue/assets/utrecht-table-row.entry-22c12614.js
+++
@@ -1,2 +0,0 @@
-import{r as o,h as r,H as s}from"./preview-18f438c3.js";import"./tiny-invariant-dd7d57d2.js";import"./component-library-vue-84bb6690.js";import"./vue.esm-bundler-759e87b8.js";import"./iframe-ad9c586a.js";import"../sb-preview/runtime.js";const e=":host{break-inside:avoid;page-break-inside:avoid;display:table-row}",a=class{constructor(t){o(this,t)}render(){return r(s,{role:"row"},r("slot",null))}};a.style=e;export{a as utrecht_table_row};
-//# sourceMappingURL=utrecht-table-row.entry-22c12614.js.map
