0d3720713d0238784a7b330db90872b5621edbe7
Robbert <mail@robbertbroersma.nl>
2023-11-06 12:34:52 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@94830f6dd37fdf169a22872d8ebeb6a9a52e5042 🚀

---
+++ storybook-vue/assets/preview-2b31b16e.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-bb62d39b.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-b9deac59.js"),["./DocsRenderer-3PUGWF3O-b9deac59.js","./chunk-S4VUQJ4A-21943a4a.js","./iframe-bb62d39b.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-8bfd3a25.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-2b31b16e.js.map
